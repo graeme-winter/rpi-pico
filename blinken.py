5ee59ff3253064cb9b1453872e35ae4acec290de
Graeme Winter <graeme.winter@gmail.com>
2021-03-17 17:25:03 +0000

    Use ersatz greyscale

--- blinken.py
+++ blinken.py
@@ -8,14 +8,10 @@ scroll.init()
 width = scroll.get_width()
 height = scroll.get_height()
 while True:
-    scroll.clear();
+    scroll.clear()
     for y in range(0, height):
         for x in range(0, width):
-            if random.random() < 0.5:
-                scroll.set_pixel(x, y, 0)
-            else:
-                scroll.set_pixel(x, y, 32)
+            scroll.set_pixel(x, y, 128 * int(random.random() * 2))
 
     scroll.update()
     time.sleep(0.2)
-
