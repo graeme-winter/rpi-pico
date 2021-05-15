3f6059bdf8c2e08e5e3bbd49e77a2cf7a19b205b
Graeme Winter <graeme.winter@gmail.com>
2021-05-15 17:02:38 +0100

    Output font image

--- font5x7/font.py
+++ font5x7/font.py
@@ -298,3 +298,32 @@ def render_view(text, offset):
     else:
         l = 1 + (17 + offset) // 6
         return bytearray(0 for j in range(-offset)) + render(text[:l])
+
+
+def render_bitmap(bitmap, filename):
+    """Render font bitmap for viewing"""
+
+    import numpy as np
+    from matplotlib import pyplot as plt
+
+    image = np.zeros(shape=(16 * 8 + 1, 16 * 6 + 1))
+
+    for c in bitmap:
+        x, y = divmod(c, 16)
+        for j in range(5):
+            bits = bin(bitmap[c][j])[2:].zfill(7)
+            for k in range(7):
+                image[8 * y + k + 1, 6 * x + j + 1] = int(bits[6 - k])
+
+    fig = plt.figure(figsize=(6, 8), frameon=False)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_frame_on(False)
+    ax.set_xlim(0, 16 * 6 + 1)
+    ax.set_ylim(16 * 8 + 1, 0)
+    ax.axis("off")
+    ax.imshow(image, cmap="Greys")
+    plt.savefig(filename, bbox_inches="tight", pad_inches=0)
+
+
+if __name__ == "__main__":
+    render_bitmap(__bitmap, "bitmap.png")
